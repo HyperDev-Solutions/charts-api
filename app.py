2c7292ab3100fb9355b12299e85473734eff4b69
Muhammad Roshaan <khanroshaan9@gmail.com>
2023-05-05 21:16:35 +0500

    done

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from datetime import datetime, timedelta
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import jwt
+import os
 from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'key123'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -13,7 +17,7 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password):
@@ -24,27 +28,28 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
 
     def to_dict(self):
-        return {'id': self.id, 'username': self.username}
+        return {'id': self.id, 'email': self.email}
     
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {}>'.format(self.email)
 
 with app.app_context():
     db.create_all()
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    username = request.json.get('username')
-    password = request.json.get('password')
+    data = request.get_json()
+    if not data or 'email' not in data or 'password' not in data:
+        return jsonify({'error': 'email and password are required'}), 400
 
-    if not username or not password:
-        return jsonify({'error': 'username and password are required'}), 400
+    email = data['email']
+    password = data['password']
 
-    if User.query.filter_by(username=username).first():
-        return jsonify({'error': 'username already exists'}), 400
+    if User.query.filter_by(email=email).first():
+        return jsonify({'error': 'email already exists'}), 400
 
-    user = User(username=username)
+    user = User(email=email)
     user.set_password(password)
     db.session.add(user)
     db.session.commit()
@@ -56,21 +61,28 @@ def get_user_count():
     user_count = User.query.count()
     return jsonify({'count': user_count})
 
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.json.get('username')
-    password = request.json.get('password')
+    data = request.get_json()
+    if not data or 'email' not in data or 'password' not in data:
+        return jsonify({'error': 'email and password are required'}), 400
 
-    user = User.query.filter_by(username=username).first()
+    email = data['email']
+    password = data['password']
+
+    user = User.query.filter_by(email=email).first()
     if not user or not user.check_password(password):
         return jsonify({'message': 'Invalid credentials'}), 401
 
     token_payload = {'sub': user.id, 'exp': datetime.utcnow() + timedelta(minutes=30)}
     token = jwt.encode(token_payload, app.config['SECRET_KEY'], algorithm='HS256')
-    
 
-    return jsonify({'token': token})
+    return jsonify({'token': token, "message": "login successful"})
 
 if __name__ == '__main__':
     app.run(debug=True)
